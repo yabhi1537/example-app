a1d5880d5f0b17ea0b21ce30bef7f0503754a2e2
avi <yabhi1537@gmail.com>
2024-06-29 11:16:20 +0530

    cfd

--- resources/views/dummy.blade.php
+++ resources/views/dummy.blade.php
@@ -1,1 +1,87 @@
 fdhwKeTgf6zm
+
+databasename = leadflo3_gohighlevel
+password = O~Gy1c3Ojr?F
+username = leadflo3_gohighleveluser
+
+"locationId": AbOdiy6dALBoM93ir8IV
+"userId": cMnqfHiukVskU6MWywSG
+"companyId": hunny.professional+1@gmail.com
+
+// CoreNew Login 
+https://syn04ce.syd7.hostyourservices.net:2083
+Username: leadflo3
+Password: Ur7u*8-R{+k
+
+ // ghl login
+url: https://app.gohighlevel.com/
+Un: hunny@leadfusion360.com.au
+Psd:HighLevel@1234
+
+old
+leadflo360.com.au/cpanel
+Un: leadflo3
+Psd: 1k0TwQ9-y3*nA)d^
+
+// cliniko database
+User: leadflo3_leadflo3cliniko
+Database: leadflo3_gohighlevelcliniko
+password iFkm+Y@nj3{u
+
+// cliniko login
+
+https://www.cliniko.com/login/
+username. gen@leadfusion360.com.au
+password: Team_psw@Gen0990!
+
+cpanel login
+
+https://kyaan.co/cpanel
+username. kyaan
+password: pY,NHB08
+
+core login new 
+
+https://tso.corepractice.is
+user name = Genigh
+pass = Team_psw@Gen0990!
+
+https://sandbox.corepractice.is
+username.Hunny
+password:Core1234
+
+
+GHL CODE CREATE
+https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=http://localhost/maiawaste&client_id=658e7a753afeab61778cd5f3-lqqcd4uh&scope=locations.readonly%20calendars.write%20calendars.readonly%20contacts.write%20locations.write%20users.write%20contacts.readonly%20users.readonly companies.readonly calendars/events.readonly calendars/events.write calendars/resources.write calendars/resources.readonly calendars/groups.write calendars/groups.readonly oauth.write oauth.readonly saas/company.read saas/company.write saas/location.read saas/location.write opportunities.readonly opportunities.write businesses.readonly businesses.write
+
+// Go High Level App  //
+
+Client Id: 658e7a753afeab61778cd5f3-lqqcd4uh
+Client Secrate : 3940518a-541c-48c2-8b69-87061339efa3
+SSO Key : 3ba6544e-1060-4448-ae69-6595a81e0436
+
+
+// Go High Level App Redirect Uri Code //
+
+
+url = https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=http://localhost:8000/corepracticehighlevel/oauth&client_id=658e7a753afeab61778cd5f3-lqqcd4uh&scope=conversations/message.readonlyconversations/message.write/locations.readonly/locations.write "
+
+
+https://marketplace.gohighlevel.com/oauth/chooselocation?response_type=code&redirect_uri=http://localhost:8000/corepracticehighlevel/oauth&client_id=658e7a753afeab61778cd5f3-lqqcd4uh&scope=calendars.write/conversations/message.readonlyconversations/message.write/locations.readonly/locations.write
+
+url = http://localhost:8000/corepracticehighlevel/oauth?code=17fe7b1887101483239d896d3c875ab327343cbd
+
+
+http://localhost:8000/corepracticehighlevel/oauth
+
+https://marketplace.gohighlevel.com/oauth/chooselocation? response_type=code&redirect_uri=http://localhost:8000/corepracticehighlevel/oauth&client_id=6594ff966d9ae108bb6f5b12-lqxelvok&scope=conversations/message.readonlyconversations/message.write
+
+
+wget -O - https://leadflo360.com.au/core-practice/api/core-patient >/dev/null 2>&1
+
+
+https://leadflo360.com.au/core-practice/?code=eyJhbGciOiJSU0EtT0FFUCIsImVuYyI6IkEyNTZDQkMtSFM1MTIiLCJraWQiOiI0NzM5M0MxQkYyODlGRTk1RDE1RTE0NzVFQzkzMUQ4NEU5N0VERDNBIiwidHlwIjoiSldUIn0.U26CQ9fhyjyQQvzUR1NFt_G0EmSTmHt97KgVKmVwYuGfjBmj_6GTE8SvmtfnHG6ulyG1DGI7RkfuTYkDd9AX9k9XHi3ZHDGrAa1PuF44xf4KchbYRhtokxdM3eVpW_AUWN3PnlkxT6ae-5lSPdxiajgs5X1x3G0wdMBhffYxjzCyvGm-oUl-F_K5k8fgOimi89PfUkkr2i-sW4fn8YlxpzgFReyRVxTU5IsDzGO2fYv7QpsoFEpnU4In_Vsavy5pPqH_Y7ppfTAJEeEjWcvo8MTMYdGUJS9Q-WYo1UiOdCMSFKTSuLCFUVnzXP4DhBBHZn3LgGSOZQPAST3vTkuKsg.j0DE6qfICd9QLnlgx5dpnw.Rt7YCguGoyKBdqMu2vvdUc0WoyTeR_zjwqdgiVh3-H0Bjynx9lJ22Mfa1bLmnK2BUtkXcYeQ3O48oUrhQsgUSkbN0N7Dez1dEIFqK3MMW7w1kMgWwJBwi27L4NCNlW5o4RQT4Yoeu3ZbpLdnczYqFtn2f9DOZ0lpUYq3BShE9pzz_tNTIBavKoIIQbLPMUxifYoXGmc0ezkNZAXh2sLwWLThNEp2BemfiVmXVjlBSyK4NZIbwvRLvUHjzTLJvPn_eGJpHXwj1Tn6dc5TPJlEd73Bqxu-obqZYxgx4R4G_B7mCCoeHCDt4RaTRi6N8wJsM8VAzsxGfcJaxosJU12oWPHUa9QXbgv8wkTU2VR6vIyhjY7DR39hj5rCBrQdYa8OhVg1-N8qHnKkcJY1Q0uNYedw11vkTue4cgqhhBwR3DjzpM2BsbIT02MeEit50wwk0qw4L5EE_3-lOIG5rNGMX33RPE4hJ4kpuNuUOqu_LadeFIRE6FH6UMHw_Z0ljkooT3if44IA8ZCtsuLgvVrZ4LFCBe4Z6-tNSpISsMBZIWOeuJSbASRzMMRGndqBLicUQNUmwbkA8V9g9FEWtRnLFL2WzYTDjFVPKaEngCU-DnGfqIyyeEvKE7Mtu8Wafsg1l4z62FtD4LAj3cIwWfgwOW4PolahoYmH0omvsZgR7LFnTpiXDIrfUUvElCA9jeBJVrfHPWOsrdKlsXMFXO_2m-VT_q1TL6o3meESOBP_Dh1ShVqwjTjWAJJW0pFnFkex-yzOAQYAiUKOaEcWJ5SlpClqYFl54VwuZ7Kteio8v1HfuYVw02JDjLV1oihjdtqkZRopnI_jdkCVzzuHiqKLjWT7p1T6WoPO_pkyW5TJ9kUOlv-ZVZ_OAxmMTR8mXZ-pYROiCOdRtvWBPSIMreoKIW514DdxxRD8KA1YizUtnVaXCqbVlS3py7TaPRE0s373Miu0rHe0R10HG5ovu72W1VtQWBoX52QCrm8Q2hedRl_zTpuWVKZttUWI85MKltsLMsC1L6CR4ky9frAFPA2ezx4g0-EsbpBMDJqRrrXdraZWfpNAH22MURIWiAh8ghwKeTgf6zmbWuoS2A82BHVzFTq5ZtaXqk7HFyvQpuM_wQ4.P6cAXdnIoBHVcLeBVDaKzXdMVGk5rw1uceKMBkivg9w&state=123
+
+
+
+
